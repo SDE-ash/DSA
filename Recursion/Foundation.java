1096bbf5ab40926530d3dd2574791d9e4b225ca6
akash <sdeveloperakash@gmail.com>
2025-04-27 19:42:56 +0530

    first coomit

--- Recursion/Foundation.java
+++ Recursion/Foundation.java
@@ -1,10 +1,25 @@
 package Recursion;
 
 public class Foundation {
+
+    public static String printNameNTimes(String name, int times){
+
+        if(times ==1){
+
+            System.err.println(name);
+            return name;
+        }
+
+        System.err.println(name);
+        printNameNTimes(name, times-1);
+        return name;
+
+    }
     public static void main(String[] args) {
 
         System.out.println("hello world");
 
-        // Your code here
+        System.err.println(printNameNTimes("akash", 5));
+        // Your code herestatic
     }
 }
