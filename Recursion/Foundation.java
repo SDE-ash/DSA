2303a1fbde203e31927905b50f7938e953a058e6
akash <sdeveloperakash@gmail.com>
2025-04-27 19:26:14 +0530

    new change

--- Recursion/Foundation.java
+++ Recursion/Foundation.java
@@ -3,7 +3,8 @@ package Recursion;
 public class Foundation {
     public static void main(String[] args) {
 
-        
+        System.out.println("hello world");
+
         // Your code here
     }
 }
